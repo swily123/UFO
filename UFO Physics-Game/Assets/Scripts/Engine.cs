6b939248a918e9762ddb08045c2442720d920214
swily123 <92625491+swily123@users.noreply.github.com>
2025-12-03 01:12:51 +0500

    added damping and stabilizers

--- UFO Physics-Game/Assets/Scripts/Engine.cs
+++ UFO Physics-Game/Assets/Scripts/Engine.cs
@@ -1,14 +1,20 @@
+using System;
 using UnityEngine;
+using Utils;
 
 public class Engine : MonoBehaviour
 {
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _spherecastRadius;
     [SerializeField] private float _maxDistance;
+    
     [SerializeField] private float _maxForce;
+    [SerializeField] private float _damping;
     
     private Transform _transform;
     private Rigidbody _targetBody;
+    private float _springSpeed;
+    private float _oldDistance;
     [SerializeField] private float _altitude;
     
     public void Initialize(Rigidbody targetBody)
@@ -33,7 +39,18 @@ public class Engine : MonoBehaviour
         {
             var distance = hitInfo.distance;
             
-            _targetBody.AddForce(-forward * (_maxForce * Mathf.Clamp01(1-distance/10f)), ForceMode.Force);
+            _springSpeed = (distance - _oldDistance) * Time.fixedTime;
+            _springSpeed = Mathf.Max(_springSpeed, 0f);
+            _oldDistance = distance;
+            
+            var minForceHeight = _altitude + 1f;
+            var maxForceHeight = _altitude - 1f;
+            distance = Mathf.Clamp(distance, maxForceHeight, minForceHeight);
+            
+            var forceFactor = distance.Remap(maxForceHeight, minForceHeight, _maxForce, 0);
+            _targetBody.AddForce(-forward * Mathf.Max(forceFactor - _springSpeed * _maxForce * _damping, 0), ForceMode.Force);
+            
+            
         }
     }
 }
---
+++ UFO Physics-Game/Assets/Scripts/Stabilizers/PositionStabilizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Stabilizers
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PositionStabilizer : MonoBehaviour
+    {
+        [SerializeField] private float _stabilizerForce;
+        
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody.AddForce(Vector3.forward * (-_rigidbody.position.z * _stabilizerForce), ForceMode.Force); //TODO добавить damping
+        }
+    }
+}
